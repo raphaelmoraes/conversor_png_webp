6408269006ab84b39cd6d038ee8106f2f7c2c755
Raphael Moraes <viladapenha@gmail.com>
2023-03-03 21:04:55 -0300

    ajustes finos

--- converter_imagem_bd.py
+++ converter_imagem_bd.py
@@ -3,6 +3,7 @@ import os
 import json
 import mysql.connector
 
+#path da localização das imagens
 path = "C:/projetos/python/conversor_png_webp/imagens/"
 
 # Conexão com o banco de dados MySQL
@@ -17,37 +18,32 @@ except mysql.connector.Error as err:
     print("Erro ao conectar no banco de dados:", err)
     exit()
 
-# Cursor para executar comandos SQL
+# abre o cursor para a consulta no banco de dados
 cursor = db.cursor()
 
-# Seleciona todas as imagens que ainda não foram convertidas
+# Seleciona todas as imagens que ainda não foram convertidas com o limite de 10
 try:
-    cursor.execute("SELECT id, caminho_imagem FROM imagens WHERE convertida = 0 limit 10")
+    cursor.execute("SELECT caminho_imagem FROM imagens WHERE convertida = 0 limit 10")
     imagens_nao_convertidas = cursor.fetchall()
 except mysql.connector.Error as err:
     print("Erro ao executar comando SQL:", err)
     db.close()
     exit()
 
-# Lista para armazenar os caminhos das imagens que serão convertidas
-caminhos_a_converter = []
-
-# Percorre cada imagem não convertida
-for (id, caminho_imagem) in imagens_nao_convertidas:
-    caminhos_a_converter.append(caminho_imagem)
-
-# Lista para armazenar as imagens que foram convertidas com sucesso
-imagens_convertidas = []
-
-# Lista para armazenar as mensagens de erro durante a conversão
-erros_conversao = []
-
 # Looping para converter cada imagem
-for imagens in caminhos_a_converter:
+for imagens in imagens_nao_convertidas:
+    
+    # array para gravar as imagens convertidas
+    imagens_convertidas = []
+    
+    #boolean de validação para saber se todo o grupo de imagens foi convertido antes de gravar
     converteu = True
-    for caminho_imagem in json.loads(imagens):
+    
+    #o caminho das imagens no banco de dados foram armazenadas em formato de array (json) será necessário fazer um loop dentro da estrutura
+    for caminho_imagem in json.loads(imagens[0]):
         try:
-            # Abre a imagem com a biblioteca Pillow
+            print(caminho_imagem)
+            # Abre a imagem com a biblioteca Pillow. Adicionado o path completo da imagem
             imagem = Image.open( path + caminho_imagem)
 
             # Define o caminho e o nome do arquivo WebP
@@ -59,11 +55,14 @@ for imagens in caminhos_a_converter:
             # Adiciona o caminho da imagem convertida à lista de imagens convertidas
             imagens_convertidas.append(caminho_webp)
         except:
+            #caso ocorra algum erro na conversão
             converteu = False
             
     try:
+        #caso tenha convertido todo o grupo de imagens
         if converteu:
-            cursor.execute("UPDATE imagens SET convertida = 1, caminho_imagem = %s WHERE caminho_imagem = %s", (json.dumps(imagens_convertidas), imagens,))
+            #realiza update no banco de dados atualizando o campo de convertido e atualiiza com os novas extensões das imagens
+            cursor.execute("UPDATE imagens SET convertida = 1, caminho_imagem = %s WHERE caminho_imagem = %s", (json.dumps(imagens_convertidas), imagens[0],))
             db.commit()
             print(f"Grupo de imagens atualizado")
     except mysql.connector.Error as err:
@@ -71,4 +70,5 @@ for imagens in caminhos_a_converter:
         db.rollback()
 
 # Fecha a conexão com o banco de dados
+cursor.close()
 db.close()
